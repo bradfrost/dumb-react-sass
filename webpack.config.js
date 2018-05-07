aefeb4de0f5c7bfa094d88c26a505e15c8d72327
Ian Frost <inf5000@gmail.com>
2018-05-07 13:02:58 -0400

    Initial sass push (WIP)

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// webpack.config.js
+module.exports = {
+	...
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS
+            ]
+        }]
+    }
+};
